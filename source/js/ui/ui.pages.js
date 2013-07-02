d4677e4fe34157355ed95b1bb92f620fb44fe249
riasky <riasky@msn.com>
2013-07-02 22:18:36 +0800

    修改分页代码

--- source/js/ui/ui.pages.js
+++ source/js/ui/ui.pages.js
@@ -25,49 +25,6 @@
  */
 
 ui.pages = ui.pages || {};
-/* --------------------------------------
-
-	1. 首页：上一页  |1|  2   3   4   5  ... 10  下一页
-
-	2. 前五：上一页   1  |2|  3   4   5  ... 10  下一页
-		    上一页   1   2  |3|  4   5  ... 10  下一页
-
-	3. 前六：上一页   1   2   3  |4|  5   6  ...  10  下一页
-
-	4. 中五：上一页   1  ...  3   4  |5|  6   7  ...   10  下一页
-			上一页   1  ...  4   5  |6|  7   8  ...   10  下一页
-
-	5. 后六：上一页   1  ...  5   6  |7|  8   9   10  下一页
-
-	6. 后五：上一页   1  ...  6   7  |8|  9   10  下一页
-			上一页   1  ...  6   7   8  |9|  10  下一页
-
-	7. 尾页：上一页   1  ...  6   7   8   9  |10|  下一页
-
-	-------------------------------------
-
-	1. 首页：page = first
-
-	2. 前五：page + 2 <= 5
-
-	3. 前六：page + 2 <= 6
-
-	4. 中五：6 < page + 2 <= length - 3
-	   		3 < page - 2 < length - 6
-
-	5. 后六：page - 2 >= length - 6
-
-	6. 后五：page - 2 >= length - 5
-
-	7. 尾页：page = length
-
- --------------------------------------- */
-
-
-
-
-
-
 
 (function(){
 
@@ -80,7 +37,7 @@ ui.pages = ui.pages || {};
 	var opt = {
 		id  	 : 'ui_pages',
 		handle   : '#ui_pages',
-		length 	 : 5,				// 连续显示的长度
+		size 	 : 5,				// 连续显示的节点数
 		bitwise  : 2,				// 当前页码左右要显示页数的长度
 		current  : 1,				// 当前页数
 		pages    : 20,				// 总页数
@@ -89,40 +46,48 @@ ui.pages = ui.pages || {};
 		prev 	 : '.pages_prev',	// 上一页的样式
 		next  	 : '.pages_next',	// 下一页的样式
 		item     : '.pages_item',
-		isMore   : true,			// 是否显示“...”
+		firstText: false,			// 第一页显示的文本: 默认显示 1
+		lastText : false,			// 最后一页显示的文本：默认显示 总页数
+		isMore   : true,			// 是否显示“...”,
+		pageClick: null,
+		prevClick: null,
+		nextClick: null,
 		callback : null 			// 点击页数时的会掉函数，返回2个参数：当前页数和总页数
 	};
+
+	pg.html = '';
+	pg.empty = function(){};
 
 	/**
 	 * 通过外部的设置来修改内部的配置；
 	 */
 	pg.config = function(o){
+
 		if(typeof(o) === 'object' && o.length === undefined){
 			$.each(o, function(k, v){
 				opt[k] = v;
 			});	
 		}
 
-		this.fn = opt.callback;
-
 		// 根据连续页数的长度计算出左右的位移数：如果length = 7 则 bitwise = 3
-		opt.bitwise = opt.length >> 1;
+		opt.bitwise = opt.size >> 1;
 
-		opt.handle.on('click', opt.item, function(){
-			var num = Number($(this).text());
-			// pg.go(num);
+		// 将设置中传入的回掉函数放到 pg.callback中；
+		// if(typeof(opt.callback) === 'function'){
+		// 	pg.callback(opt.callback);
+		// }
+
+		// 事件绑定
+		pg.fn({
+			pageClick : opt.pageClick,
+			prevClick : opt.prevClick,
+			nextClick : opt.nextClick
 		});
-
-		opt.handle.on('click', opt.prev, function(){
-			opt.current--;
-			// pg.go(opt.current);
-		});
-
-		opt.handle.on('click', opt.next, function(){
-			opt.current++;
-			// pg.go(opt.current);
-		});
-
+		
+		if(opt.current > 0 && opt.pages > 0){
+			this.go(opt.current);
+		}
+		// console.log(opt);
 		return this;
 	};
 
@@ -132,7 +97,7 @@ ui.pages = ui.pages || {};
 	 */
 	var tpl = {
 		wrap 	: '<ol class="ui_pages"></ol>',
-		page    : '<li class="pages_item"><a href="javascript:void(0);">$pageText$</a></li>',
+		page    : '<li class="pages_item" data-page="$page$"><a href="javascript:void(0);">$pageText$</a></li>',
 		more    : '<li class="pages_more"><span>...</span></li>',
 		current : '<li class="pages_current"><span>$pageText$</span></li>',
 		prev 	: '<li class="pages_prev"><a href="javascript:void(0);" class="btn btn_gray $disable$">' + opt.prevText + '</a></li>',
@@ -142,13 +107,17 @@ ui.pages = ui.pages || {};
 	/**
 	 * 返回分页节点
 	 * @param  {Number} page 页数
+	 * @param  {string} text 显示的文字
 	 * @param  {String} tp   节点类型：当前页(current) / 普通页(page)
 	 * @return {HTML}        节点HTML
 	 */
-	pg.createItem = function(page, tp){
+	pg.createItem = function(page, text, tp){
 		var tp = tp === 'current' ? 'current' : 'page';
-		var page = tpl[tp].replace('$pageText$', page);
-		return page;
+		var text = text || page;
+		var html = tpl[tp].replace('$pageText$', text);
+		html = html.replace('$page$', page);
+
+		return html;
 	};
 
 	/**
@@ -174,9 +143,6 @@ ui.pages = ui.pages || {};
 		return btn;
 	};
 
-
-
-
 	/**
 	 * 返回所有分页HTML结构
 	 * @param  {Number} current 当前页数
@@ -194,6 +160,7 @@ ui.pages = ui.pages || {};
 		}else{
 			opt.pages = 1;
 		}
+
 		// 校验当前页数
 		if(current > 0){
 			current = current > pages ? pages : current;
@@ -204,17 +171,16 @@ ui.pages = ui.pages || {};
 
 		//只有1页的时候只显示1
 		if(pages === 1){
-			return pg.createItem(1);
+			return pg.createItem(1, 1, 'current');
 		}
 
 		/* 
 			----------------------\  开始计算分页  /----------------------
-								   \_____  _____/
-								         \/
+                                   \_____  _____/
+                                         \/
 		*/
 		
 		var start, more, items, end, prev, next, length, first, last, html;
-
 
 		// 上一页节点：第一页只读
 		prev  = this.createBtn('prev', (current === 1 ? true : false));
@@ -234,31 +200,31 @@ ui.pages = ui.pages || {};
 		more  = this.createMore();
 		
 		// 第一个：前五个不显示
-		first = (end <= opt.length) ? '' : this.createItem(1);
+		first = (end <= opt.size) ? '' : this.createItem(1, opt.firstText);
 		
 		// 最后一个：后五个不显示
-		last  = (start > (pages - opt.length)) ? '' : this.createItem(pages);
+		last  = (start > (pages - opt.size)) ? '' : this.createItem(pages, opt.lastText);
 
 		// pages < 10 全显示
 		if(pages <= 10){
 			html = prev;
 			for(var i = 1; i <= pages ; i++){
 				var tp = i === current ? 'current' : false;
-				html += this.createItem(i, tp);
+				html += this.createItem(i, i, tp);
 			}
 			html += next;
 			return html;
 		}
 
 		// 前五：
-		if(end <= opt.length){
+		if(end <= opt.size){
 			start = 1;
-			end = opt.length;
+			end = opt.size;
 		}
 
 		// 后五：page - 2 >= length - 5
-		if(start > (pages - opt.length)){
-			start = (pages - opt.length) + 1;
+		if(start > (pages - opt.size)){
+			start = (pages - opt.size) + 1;
 			end = pages;
 		}
 
@@ -266,7 +232,7 @@ ui.pages = ui.pages || {};
 		items = '';
 		for(var i = start; i <= end; i++){
 			var tp = i === current ? 'current' : false;
-			items += this.createItem(i, tp);
+			items += this.createItem(i, i, tp);
 		}
 
 		// ------------ 开始拼凑分页 ------------
@@ -274,12 +240,12 @@ ui.pages = ui.pages || {};
 		html = prev + first;
 
 		// 前置省略号：如果前面的长度是（连续页数 + 1） 则不显示；
-		html += (end <= opt.length + 1) ? '' : more;
+		html += (end <= opt.size + 1) ? '' : more;
 
 		html += items;
 
 		// 后置省略号：如果最后剩余的是长度是（连续页数）则不显示；
-		html += (start >= (pages - (opt.length))) ? '' : more;
+		html += (start >= (pages - (opt.size))) ? '' : more;
 
 		html += last + next;
 
@@ -294,16 +260,112 @@ ui.pages = ui.pages || {};
 	 * @param  {Number} pages   总页数，可选
 	 * @return {[type]}         [description]
 	 */
-	pg.go = function(currnet, pages){
-		var pages = pages || opt.pages;
-		var list = this.createPages(currnet, pages);
-		var wrap = $(tpl.wrap);
-		var html = wrap.html(list);
-		// if(this.fn != null){
-		// 	// this.fn(num, pages);
-		// }
+	pg.go = function(current, pages){
+
+		opt.current = (current > 0) ? current : opt.current;
+		opt.pages = pages || opt.pages;
+
+		var html = this.getPagesHTML()
+		this.append(html);
+	};
+
+	/**
+	 * 向页面加入HTML分页代码
+	 * @param  {HTML} html [description]
+	 * @return {[type]}      [description]
+	 */
+	pg.append = function(html){
+		var html = html || this.html;
 		$(opt.handle).html(html);
 	};
+
+	/**
+	 * 返回整个分页的HTML内容
+	 * @return {[type]} [description]
+	 */
+	pg.getPagesHTML = function(){
+		var list = this.createPages(opt.current, opt.pages);
+		var wrap = $(tpl.wrap);
+		var html = wrap.html(list);
+		
+		this.html = html;
+		return html;
+	};
+
+	/**
+	 * 事件绑定
+	 * @param  {Object} option 支持3种事件：{
+	 *                         				pageClick : function(){},
+	 *                         				prevClick : function(){},
+	 *                         				nextClick : function(){}
+	 * 										}
+	 * @return {Number}    opt.current    每个事件都会返回点击后的当前页数
+	 */
+	pg.fn = function(option){
+
+		var o = {
+			// 分页节点的点击事件
+			pageClick : function(num){ pg.go(num); },
+			// 上一页的点击事件
+			prevClick : function(num){ pg.go(num); },
+			// 下一页的点击事件
+			nextClick : function(num){ pg.go(num); }
+		};
+		
+		// 根据option修改默认事件
+		if(typeof(option) === 'object' && option.length === undefined){
+			$.each(option, function(k, v){
+				// 如果没有则使用默认事件
+				if(v){
+					o[k] = v;					
+				}
+			});	
+		}
+
+		// 如果在设置参数中存在callback，则直接用opt.callback 覆盖 o.pageClick
+		if(typeof opt.callback === 'function'){
+			o.pageClick = opt.callback;
+		}
+
+		// 分页节点点击事件
+		opt.handle.on('click', opt.item, function(){
+			var currentNumber = $(this).data('page');
+			o.pageClick(currentNumber);
+		});
+		// 上一页点击事件
+		opt.handle.on('click', opt.prev, function(){
+			opt.current--;
+			var currentNumber = 1;
+			if(opt.current > 0){
+				currentNumber = opt.current;
+			}
+			o.prevClick(currentNumber);
+		});
+		// 下一页点击事件
+		opt.handle.on('click', opt.next, function(){
+			opt.current++;
+			var currentNumber = opt.pages;
+			if(opt.current <= opt.pages){
+				currentNumber = opt.current;	
+			}
+			o.nextClick(currentNumber);
+		});
+	};
+
+	/**
+	 * 自定义点击节点的回掉函数
+	 * @param  {Function}    fn             回掉函数
+	 * @return {Number}      opt.current    当前页数
+	 * @return {jQuery DOM}  data 		    返回jquery构造的分页节点HTML
+	 */
+	pg.callback = function(fn){
+		if(typeof fn === 'function'){
+			var html = pg.getPagesHTML();
+			fn(opt.current, html);
+		}
+	};
+
+
 
 })();
 
