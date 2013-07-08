b6a1f264e50553a3e80d63db96cb0b680d308e27
riasky <riasky@msn.com>
2013-07-08 21:05:58 +0800

    增加交互代码

---
+++ source/script/xue.message.min.js
@@ -0,0 +1,26 @@
+/**
+ * XESUI 
+ * Copyright 2013 xueersi.com All rights reserved.
+ *
+ * @description 
+ *
+ * @author Marco (marco@xesui.com)
+ * @modify 2013-07-08 17:01:57
+ * @version $Id$
+ * 
+ * @links http://xesui.com
+ */
+
+
+/**
+ * @name ui.message.js
+ * @description 信息
+ * 
+ * @module 
+ * @submodule 
+ * @main 
+ * @class 
+ * @constructor 
+ * @static 
+ */
+
